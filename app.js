53cda4ec9cbdb7170e9bcd2c9ca96f5b3578a5fd
Tracie <traciecodes@gmail.com>
2022-05-18 21:20:31 -0500

    Adds editing functionality to existing campgrounds

--- app.js
+++ app.js
@@ -52,6 +52,11 @@ app.get('/campgrounds/:id', async (req, res) => {
   res.render('campgrounds/show', { campground });
 });
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/edit', { campground });
+});
+
 app.listen(3000, () => {
   console.log('Serving on PORT 3000');
 });
