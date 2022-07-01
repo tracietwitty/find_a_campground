f9055845e8c4d6904b3bf896e1cac595812954ba
Tracie <traciecodes@gmail.com>
2022-07-01 13:28:14 -0500

    Adds images to each camp and creates cards on main list of camps

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
+const axios = require('axios');
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
@@ -16,21 +17,42 @@ db.once('open', () => {
 // pick a random place and descriptor from seedHelper:
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+// pick a random image from Unsplash to display with the campground:
+async function seedImg() {
+  try {
+    const resp = await axios.get('https://api.unsplash.com/photos/random', {
+      params: {
+        client_id: '7s3agHLsxNKMkuWI15Q_ZmJsO28SbqMYY9y7tw62Im4',
+        collections: 8667598,
+      },
+    });
+    return resp.data.urls.small;
+  } catch (err) {
+    console.error('This is working, but: ', err);
+  }
+}
 // async function that makes sure we can connect to db:
 const seedDB = async () => {
   // delete previous entries:
   await Campground.deleteMany({});
   // loop over cites 50 times to create dynamic dummy data:
-  for (let i = 0; i < 50; i++) {
-    // pick a random number to grab a city
-    const random1000 = Math.floor(Math.random() * 1000);
-    // create new campground and set the location to be the city, state:
-    const camp = new Campground({
-      location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      // use the random place and descriptor from 'sample':
-      title: `${sample(descriptors)} ${sample(places)}`,
+  for (let i = 0; i < 20; i++) {
+    // setup
+    const placeSeed = Math.floor(Math.random() * places.length);
+    const descriptorsSeed = Math.floor(Math.random() * descriptors.length);
+    const citySeed = Math.floor(Math.random() * cities.length);
+
+    // seed data into campground
+    const campground = new Campground({
+      title: `${descriptors[descriptorsSeed]} ${places[placeSeed]}`,
+      location: `${cities[citySeed].city}, ${cities[citySeed].state}`,
+      image: await seedImg(),
+      description:
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, nihil tempora vel aspernatur quod aliquam illum! Iste impedit odio esse neque veniam molestiae eligendi commodi minus, beatae accusantium, doloribus quo!',
+      price: Math.floor(Math.random() * 25),
     });
-    await camp.save();
+
+    await campground.save();
   }
 };
 
